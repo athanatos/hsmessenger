96077e07843ec9d11b4b47e0cde48a6b595f6ca0
Samuel Just <sam.just@inktank.com>
2013-03-16 23:47:27 -0700

    working

--- TCPTransport.hs
+++ TCPTransport.hs
@@ -3,13 +3,13 @@ module TCPMessenger (
                     ) where
 
 import qualified Transport as T
+import qualified Network.Socket.ByteString as BSS
 import qualified Network.Socket as S
 import Data.Int
 import Data.ByteString as BS
 import qualified Control.Concurrent.STM as STM
 import Data.Maybe
 import qualified Data.Map as M
-import qualified GHC.IO.Handle as H
 import qualified System.IO as SIO
 
 import qualified Channel as C
@@ -25,18 +25,17 @@ data TCPConnection =
   TCPConnection { connStatus :: STM.TVar Status
                 , connPeer :: Entity
                 , connQueue :: C.Channel ByteString
-                , connSocket :: H.Handle
+                , connSocket :: S.Socket
                 }
 
 makeConnection :: (S.Socket, S.SockAddr) -> IO TCPConnection
 makeConnection (socket, addr) = do
   status <- STM.atomically $ STM.newTVar Open
   queue <- STM.atomically $ C.makeChannel 100
-  handle <- S.socketToHandle socket SIO.ReadWriteMode
   return $ TCPConnection { connStatus = status
                          , connPeer = Entity { entityAddr = addr }
                          , connQueue = queue
-                         , connSocket = handle
+                         , connSocket = socket
                          }
 
 queueOnConnection :: TCPConnection -> ByteString -> STM.STM ()
@@ -46,7 +45,7 @@ queueOnConnection conn msg = do
 sendNextMessage :: TCPConnection -> IO ()
 sendNextMessage conn = do
   nextMsg <- STM.atomically $ C.getItem (connQueue conn)
-  BS.hPut (connSocket conn) nextMsg
+  BSS.send (connSocket conn) nextMsg
   return ()
 
 data TCPTransport =
